d16a752e0528dd6bc7bc814ea4d2107143831330
hd41 <jarvis1247@gmail.com>
2018-05-01 16:54:15 +0530

    asd

--- manage.py
+++ manage.py
@@ -7,5 +7,5 @@ manager = Manager(app)
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
-	app.run(debug = True, host='127.0.0.1',port = port)
+	app.run(debug = True, host='https://mavrickz.herokuapp.com',port = port)
 	# manager.run()
