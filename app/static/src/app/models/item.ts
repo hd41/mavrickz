bfa194d270f8305038cf4d676d928ddffdbbc791
hd41 <jarvis1247@gmail.com>
2018-05-27 12:21:25 +0530

    conversion to flask

--- app/static/src/app/models/item.ts
+++
@@ -1,8 +0,0 @@
-export class Item {
-  name:string;
-  description:string;
-  author: string ;
-  category : string;
-  likes : string;
-  constructor(name?:string, description?:string, author?: string, category?: string, likes?: string) {}
-}
--- app/static/src/app/models/user.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-  fname:string;
-  lname:string;
-  email: string ;
-  password : string;
-  constructor(fname?:string, lname?:string, email?: string, password?: string) {}
-}
